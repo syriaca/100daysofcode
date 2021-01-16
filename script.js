f5f0d9834e436c7fdf6a741de180995b2feb6eec
jean-yves le cabellec <jean-yves.lecabellec@leetchi.com>
2021-01-17 00:54:36 +0100

    First commit for chunky monkey

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 // Query DOM
 const result = document.querySelector(".result");
-const wordForm = document.querySelector(".word-form");
+const form = document.querySelector(".form");
 
-const mutation = (arr) => {
-  let firstWord = arr[0].toLowerCase().split("");
-  let secondWord = arr[1].toLowerCase().split("");
-  return secondWord.every(letter => firstWord.indexOf(letter) != -1);
+// Chunk array into a new array
+const chunkArrayInGroups = (arr = [1,2,3,4,5,6], size = 2) => {
+ 
+  	let newArray = []; 
+    
+  	while (arr.length) {
+    	newArray.push(arr.splice(0, size));
+	}
+	console.log(newArray);
+  	return newArray;
 }
 
-wordForm.addEventListener('submit', e => {
+// Listen to form submit event
+form.addEventListener("submit", e => {
 	e.preventDefault();
-	let firstWord = e.target.firstWord.value.trim();
-	let secondWord = e.target.secondWord.value.trim();
-	let mutationResult = mutation([firstWord, secondWord]);
-	let alertColor = mutationResult ? "green" : "red";
-	result.classList.remove("hidden");
-	
-	result.innerHTML = `<p class="p-5 rounded bg-${alertColor}-300 text-${alertColor}-700"><strong>${firstWord}</strong> ${mutationResult ? "contains" : "doesn't contain"} all of the letters of <strong>${secondWord}</strong></p>`;
-	wordForm.reset();
-	setTimeout(() => {
+	let arrayToChunk = e.target.test.value.split(",").map(x => Number(x));
+	let chunkSize = Number(e.target.chunkSize.value.trim());
+	let chunkResult = chunkArrayInGroups(arrayToChunk, chunkSize);
+	console.log(chunkResult);
+
+	form.classList.toggle("hidden");
+	result.classList.toggle("hidden");
+
+	result.innerHTML = `
+			<p class="my-5">
+				This is your result: [${chunkResult}]
+			</p>
+			<ul class="list-disc list-inside">
+				<li>
+					Your Array was ${arrayToChunk}
+				</li>
+				<li>
+					Your chunk size was ${chunkSize}
+				</li>
+			</ul>
+			<button class="w-55 rounded p-4 bg-green-400 text-white button-chunk-again">
+                Chunk it again !
+            </button>
+			`;
+	form.reset();
+});
+
+// Listen to button that doesn't exist yet
+result.addEventListener("click", e => {
+	if (e.target.classList.contains("button-chunk-again")) {
+		form.classList.toggle('hidden');
+		result.classList.toggle('hidden');
 		result.innerHTML = "";
-	}, 3000)
-})
+	}
+});
 
