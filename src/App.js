ffc1af22d92e84b4dd998a5223ddf919f87586ce
jean-yves le cabellec <jean-yves.lecabellec@leetchi.com>
2021-01-28 15:54:55 +0100

    add first html idea

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div>Boilerplate</div>
+        // <div className="panel" style="background-image: url('https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')">
+        //   <h3>Wild Forest</h3>
+        // </div>
+      <div className="container">
+        <div className="panel active">
+          <h3>Explore The World</h3>
+        </div>
+    </div>
   );
 }
 
