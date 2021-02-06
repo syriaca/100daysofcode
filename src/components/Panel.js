4b71e7cc7b84f18e73739b3d45fca8f3e0764789
jean-yves le cabellec <jean-yves.lecabellec@leetchi.com>
2021-02-06 07:47:09 +0100

    Add panel last styles

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -14,11 +14,31 @@ const StyledPanel = styled.div`
     flex: 0.5;
     margin: 10px;
     position: relative;
-    transition: flex;
+    transition: flex .7s ease-in;
+
+    &.active {
+        flex: 5;
+
+        h3 {
+            opacity: 1;
+        }
+    }
+
+    @media(max-width: 480px) {
+        &:nth-of-type(4),
+        &:nth-of-type(5) {
+            display: none;
+        }
+    }
 `;
 
 const StyledPanelTitle = styled.h3`
-
+    bottom: 20px;
+    font-size: 1.5rem;
+    position: absolute;
+    left: 20px;
+    margin: 0;
+    opacity: 0;
 `;
 
 const Panel = ({ key, url, heading }) => {
